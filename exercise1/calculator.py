2d53b160c374f65cf89c1ea54c99b7c5257ee78a
Sette-Th <sette.toure@centrale-casablanca.ma>
2025-03-10 14:52:54 +0000

    Exo1/Sette-Toure

--- exercise1/calculator.py
+++ exercise1/calculator.py
@@ -6,21 +6,13 @@ Number = Union[int, float]
 
 
 def add(a: Number, b: Number) -> Number:
-    """
-    Return the sum of a and b.
-
-    Args:
-        a: First number
-        b: Second number
-
-    Returns:
-        The sum of a and b
-    """
+    return a + b  
     # TODO: Implement this function
     pass
 
 
 def subtract(a: Number, b: Number) -> Number:
+    return a - b
     """
     Return the result of subtracting b from a.
 
